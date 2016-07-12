0084d63a6e60070326f2d4f59bc6e6a1f6d325b8
Vivek Krishnakumar <vkrishna@jcvi.org>
2016-07-12 15:45:10 -0400

    + Adds Medicago-Arabidopsis homologs source + Adds Phytozome web-services based homologs widget + Adds the JBrowse displayer + Updates the page header menu + Updates the report page aspects.xml

---
+++ webapp/resources/webapp/js/medicmine-options.js
@@ -0,0 +1,5 @@
+(jQuery(function() { // run when the page has loaded.
+    if (intermine.options) {   // but only if there is something to do.
+        intermine.setOptions({CellPreviewTrigger: 'hover'});
+    }
+}));
